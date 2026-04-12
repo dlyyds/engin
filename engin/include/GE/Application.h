93ee2c6d4374f88fd89f78c1222312dc81d16b51
gcw_18dE45IK <gcw_18dE45IK@noreply.gitcode.com>
2026-04-12 20:22:39 +0800

    绑定窗口时间

--- engin/include/GE/Application.h
+++ engin/include/GE/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Core.h"
+#include "Event/ApplicationEvent.h"
 #include "GEWindow.h"
 
 #include <memory>
@@ -12,10 +13,12 @@ public:
   Application();
   virtual ~Application();
   void Run();
+  void OnEvent(Event &e);
 
 private:
   std::unique_ptr<Window> m_Window;
   bool m_Running = true;
+  bool OnWindowClose(WindowCloseEvent &e);
 };
 
 Application *CreateApplication();
--- engin/src/Application.cpp
+++ engin/src/Application.cpp
@@ -4,6 +4,7 @@
 #include "pch.h"
 #include <Event/ApplicationEvent.h>
 #include <GLFW/glfw3.h>
+#include <functional>
 #include <gl/gl.h>
 #include <memory>
 #include <regex>
@@ -13,6 +14,8 @@ namespace GE {
 Application::Application() {
 
   m_Window = std::unique_ptr<Window>(Window::Create());
+  m_Window->SetEventCallback(
+      std::bind(&Application::OnEvent, this, std::placeholders::_1));
 }
 Application::~Application() {}
 void Application::Run() {
@@ -22,6 +25,18 @@ void Application::Run() {
     glClear(GL_COLOR_BUFFER_BIT);
     m_Window->OnUpdate();
   }
+}
+
+void Application::OnEvent(Event &e) {
+  EventDispatcher dispatcher(e);
+  dispatcher.Dispatch<WindowCloseEvent>(
+      std::bind(&Application::OnWindowClose, this, std::placeholders::_1));
+  GE_CORE_INFO(e);
+}
+
+bool Application::OnWindowClose(WindowCloseEvent &e) {
+  m_Running = false;
+  return true;
 }
 
 } // namespace GE
