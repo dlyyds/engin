842d18bb2245be39766983b8e56cd81e174e5ee2
gcw_18dE45IK <gcw_18dE45IK@noreply.gitcode.com>
2026-04-12 19:15:24 +0800

    加入glfw 抽象window

--- engin/src/Application.cpp
+++ engin/src/Application.cpp
@@ -1,20 +1,26 @@
 #include "Application.h"
+#include "GEWindow.h"
 #include "Log.h"
 #include "pch.h"
 #include <Event/ApplicationEvent.h>
+#include <GLFW/glfw3.h>
+#include <gl/gl.h>
+#include <memory>
+#include <regex>
 
 namespace GE {
 
-Application::Application() {}
+Application::Application() {
+
+  m_Window = std::unique_ptr<Window>(Window::Create());
+}
 Application::~Application() {}
 void Application::Run() {
-  // std::cout << "hello\n";
-  Log::Init();
-  GE_CORE_INFO("Initialized log");
-  GE_INFO("Hello");
-  WindowResizeEvent e(1000, 1000);
-  GE_TRACE(e);
-  while (true) {
+
+  while (m_Running) {
+    glClearColor(1, 0, 1, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    m_Window->OnUpdate();
   }
 }
 
