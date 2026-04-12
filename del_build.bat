2667d7ff9c4308a157da28ca88340bb2e376f078
dlyyds <16030437+dlyyds@user.noreply.gitee.com>
2026-04-12 15:43:15 +0800

    pch

--- del_build.bat
+++ del_build.bat
@@ -6,6 +6,12 @@ echo ==============================
 echo 开始构建 GameEngine
 echo ==============================
 
+:: 1. 删除 bin 文件夹
+if exist bin (
+    echo 删除旧的 bin 文件夹...
+     rmdir /s /q bin
+     rmdir /s /q bin-int
+)
 
 :: 2. CMake 生成
 echo CMake 生成工程...
---
+++ engin/include/GE/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <utility>
+
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "spdlog/common.h"
+#include "spdlog/spdlog.h"
+#include <spdlog/fmt/ostr.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#ifdef HZ_PLATFORM_WINDOWS
+#include <Windows.h>
+#endif
