d9248a021d34412b70e87982c7afcd8397d09411
gcw_18dE45IK <gcw_18dE45IK@noreply.gitcode.com>
2026-04-13 21:36:02 +0800

    添加imgui

--- engin/src/Application.cpp
+++ engin/src/Application.cpp
@@ -11,8 +11,11 @@
 
 namespace GE {
 
+Application *Application::s_Instance = nullptr;
 Application::Application() {
 
+  GE_CORE_ASSERT(!s_Instance, "Application already exists!");
+  s_Instance = this;
   m_Window = std::unique_ptr<Window>(Window::Create());
   m_Window->SetEventCallback(
       std::bind(&Application::OnEvent, this, std::placeholders::_1));
@@ -48,8 +51,14 @@ bool Application::OnWindowClose(WindowCloseEvent &e) {
   return true;
 }
 
-void Application::PushLayer(Layer *layer) { m_LayerStack.PushLayer(layer); }
+void Application::PushLayer(Layer *layer) {
+  m_LayerStack.PushLayer(layer);
+  layer->OnAttach();
+}
 
-void Application::PushOverlay(Layer *layer) { m_LayerStack.PushOverlay(layer); }
+void Application::PushOverlay(Layer *layer) {
+  m_LayerStack.PushOverlay(layer);
+  layer->OnAttach();
+}
 
 } // namespace GE
