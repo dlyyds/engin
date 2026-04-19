9a3315ff9642677d887428f1678e9a92d6ab2e0d
gcw_18dE45IK <gcw_18dE45IK@noreply.gitcode.com>
2026-04-19 17:37:32 +0800

    Bufferlayout

--- engin/include/GE/platform/OpenGl/OpenGLBuffer.h
+++ engin/include/GE/platform/OpenGl/OpenGLBuffer.h
@@ -8,9 +8,12 @@ public:
   ~OpenGLVertexBuffer();
   void Bind() const override;
   void Unbind() const override;
+  void SetLayout(const BufferLayout &layout) override { m_layout = layout; }
+  const BufferLayout &GetLayout() const override { return m_layout; }
 
 private:
   uint32_t m_RendererID;
+  BufferLayout m_layout;
 };
 
 class OpenGLIndexBuffer : public IndexBuffer {
