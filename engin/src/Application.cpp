bb016f771c5abf2be7f391c24d4e9b0d0d1f4a5d
gcw_18dE45IK <gcw_18dE45IK@noreply.gitcode.com>
2026-04-19 12:24:11 +0800

    第一个三角形

--- engin/src/Application.cpp
+++ engin/src/Application.cpp
@@ -8,10 +8,12 @@
 #include "KeyCodes.h"
 #include "MouseButtonCodes.h"
 
+#include <glad/glad.h>
+
 #include <Event/ApplicationEvent.h>
+
 #include <GLFW/glfw3.h>
 #include <functional>
-#include <gl/gl.h>
 #include <memory>
 #include <regex>
 
@@ -20,62 +22,88 @@ namespace GE {
 Application *Application::s_Instance = nullptr;
 Application::Application() {
 
-    GE_CORE_ASSERT(!s_Instance, "Application already exists!");
-    s_Instance = this;
-    m_Window = std::unique_ptr<Window>(Window::Create());
-    m_Window->SetEventCallback(std::bind(&Application::OnEvent, this, std::placeholders::_1));
+  GE_CORE_ASSERT(!s_Instance, "Application already exists!");
+  s_Instance = this;
+  m_Window = std::unique_ptr<Window>(Window::Create());
+  m_Window->SetEventCallback(
+      std::bind(&Application::OnEvent, this, std::placeholders::_1));
 
-    m_ImGuiLayer = new ImGuiLayer();
-    PushOverlay(m_ImGuiLayer);
+  m_ImGuiLayer = new ImGuiLayer();
+  PushOverlay(m_ImGuiLayer);
+
+  glGenVertexArrays(1, &m_VertexArray);
+  glBindVertexArray(m_VertexArray);
+
+  glGenBuffers(1, &m_VertexBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+  float vertices[3 * 3] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+                           0.0f,  0.0f,  0.5f, 0.0f};
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+  glGenBuffers(1, &m_IndexBuffer);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+  unsigned int indices[3] = {0, 1, 2};
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
 }
 Application::~Application() {}
 void Application::Run() {
 
-    while (m_Running) {
-        glClearColor(1, 0, 1, 1);
-        glClear(GL_COLOR_BUFFER_BIT);
+  while (m_Running) {
+    glClearColor(0.1f, 0.1f, 0.1f, 0.1);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-        for (Layer *layer : m_LayerStack)
-            layer->OnUpdate();
+    glBindVertexArray(m_VertexArray);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
-        m_ImGuiLayer->Begin();
-        for (Layer *layer : m_LayerStack)
-            layer->OnImGuiRender();
-        m_ImGuiLayer->End();
+    for (Layer *layer : m_LayerStack)
+      layer->OnUpdate();
 
-        m_Window->OnUpdate();
-    }
+    m_ImGuiLayer->Begin();
+    for (Layer *layer : m_LayerStack)
+      layer->OnImGuiRender();
+    m_ImGuiLayer->End();
+
+    m_Window->OnUpdate();
+  }
 }
 
 void Application::OnEvent(Event &e) {
-    EventDispatcher dispatcher(e);
-    dispatcher.Dispatch<WindowCloseEvent>(std::bind(&Application::OnWindowClose, this, std::placeholders::_1));
-    GE_CORE_TRACE("Application::OnEvent {0}", e);
+  EventDispatcher dispatcher(e);
+  dispatcher.Dispatch<WindowCloseEvent>(
+      std::bind(&Application::OnWindowClose, this, std::placeholders::_1));
+  //  GE_CORE_TRACE("Application::OnEvent {0}", e);
 
-    if (Input::IsKeyPressed(GE_KEY_TAB)) {
-        GE_CORE_TRACE("Tab pressed");
-    }
+  // if (Input::IsKeyPressed(GE_KEY_TAB)) {
+  //     GE_CORE_TRACE("Tab pressed");
+  // }
 
-    for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();) {
-        (*--it)->OnEvent(e);
-        if (e.Handled)
-            break;
-    }
+  for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();) {
+    (*--it)->OnEvent(e);
+    if (e.Handled)
+      break;
+  }
 }
 
 bool Application::OnWindowClose(WindowCloseEvent &e) {
-    m_Running = false;
-    return true;
+  m_Running = false;
+  return true;
 }
 
 void Application::PushLayer(Layer *layer) {
-    m_LayerStack.PushLayer(layer);
-    layer->OnAttach();
+  m_LayerStack.PushLayer(layer);
+  layer->OnAttach();
 }
 
 void Application::PushOverlay(Layer *layer) {
-    m_LayerStack.PushOverlay(layer);
-    layer->OnAttach();
+  m_LayerStack.PushOverlay(layer);
+  layer->OnAttach();
 }
 
 } // namespace GE
