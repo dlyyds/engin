3ff1ec461e5c536025690bb645859bd64acfedea
gcw_18dE45IK <gcw_18dE45IK@noreply.gitcode.com>
2026-04-21 23:11:22 +0800

    Orthographic camera

--- engin/include/GE/Renderer/Shader.h
+++ engin/include/GE/Renderer/Shader.h
@@ -1,15 +1,16 @@
 
 #pragma once
-
+#include "glm/glm.hpp"
 namespace GE {
 class Shader {
-public:
-  Shader(const std::string &vertexSrc, const std::string &fragmentSrc);
-  ~Shader();
-  void Bind();
-  void Unbind();
+  public:
+    Shader(const std::string &vertexSrc, const std::string &fragmentSrc);
+    ~Shader();
+    void Bind();
+    void Unbind();
+    void UploadUniformMat4(const std::string name, const glm::mat4 &matrix);
 
-private:
-  uint32_t m_RendererID;
+  private:
+    uint32_t m_RendererID;
 };
 } // namespace GE
---
+++ engin/src/Renderer/OrthographicCamera.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+
+#include "Renderer/OrthographicCamera.h"
+#include "glm/gtc/matrix_transform.hpp"
+
+namespace GE {
+OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top) {
+    m_ProjectionMatrix = glm::ortho(left, right, bottom, top);
+    m_ViewMatrix = glm::mat4(1);
+    m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+}
+
+void OrthographicCamera::RecalculateViewMatrix() {
+
+    float radians = glm::radians(m_Rotation);
+    glm::mat4 transform = glm::translate(glm::mat4(1), m_Position) *
+                          glm::rotate(glm::mat4(1), radians, glm::vec3(0, 0, 1));
+    m_ViewMatrix = glm::inverse(transform);
+    m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+}
+
+} // namespace GE
