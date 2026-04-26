a1f006bd81e0eb05b13a50fda0dcae6f68e86f27
gcw_18dE45IK <gcw_18dE45IK@noreply.gitcode.com>
2026-04-26 23:02:42 +0800

    Sandbox2D

---
+++ sandbox/Sandbox2D.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "GE/GE.h"
+
+class Sandbox2D : public GE::Layer {
+  public:
+    Sandbox2D();
+    virtual ~Sandbox2D() = default;
+
+    virtual void OnAttach() override;
+    virtual void OnDetach() override;
+
+    void OnUpdate(GE::Timestep &ts) override;
+    virtual void OnImGuiRender() override;
+    void OnEvent(GE::Event &e) override;
+
+  private:
+    GE::OrthographicCameraController m_CameraController;
+
+    // Temp
+    GE::Ref<GE::VertexArray> m_SquareVA;
+    GE::Ref<GE::Shader> m_FlatColorShader;
+
+    glm::vec4 m_SquareColor = {0.2f, 0.3f, 0.8f, 1.0f};
+};
