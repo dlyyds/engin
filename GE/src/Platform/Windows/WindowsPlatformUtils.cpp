d44e91b5f82e0927bd0f16fea040393447482384
gcw_18dE45IK <gcw_18dE45IK@noreply.gitcode.com>
2026-05-13 22:08:51 +0800

    优化windows文件对话框

--- GE/src/Platform/Windows/WindowsPlatformUtils.cpp
+++ GE/src/Platform/Windows/WindowsPlatformUtils.cpp
@@ -13,11 +13,14 @@ namespace GE {
 std::optional<std::string> FileDialogs::OpenFile(const char *filter) {
     OPENFILENAMEA ofn;
     CHAR szFile[260] = {0};
+    CHAR currentDir[256] = {0};
     ZeroMemory(&ofn, sizeof(OPENFILENAME));
     ofn.lStructSize = sizeof(OPENFILENAME);
     ofn.hwndOwner = glfwGetWin32Window((GLFWwindow *)Application::Get().GetWindow().GetNativeWindow());
     ofn.lpstrFile = szFile;
     ofn.nMaxFile = sizeof(szFile);
+    if (GetCurrentDirectoryA(256, currentDir))
+        ofn.lpstrInitialDir = currentDir;
     ofn.lpstrFilter = filter;
     ofn.nFilterIndex = 1;
     ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
@@ -30,16 +33,19 @@ std::optional<std::string> FileDialogs::OpenFile(const char *filter) {
 std::optional<std::string> FileDialogs::SaveFile(const char *filter) {
     OPENFILENAMEA ofn;
     CHAR szFile[260] = {0};
+    CHAR currentDir[256] = {0};
     ZeroMemory(&ofn, sizeof(OPENFILENAME));
     ofn.lStructSize = sizeof(OPENFILENAME);
     ofn.hwndOwner = glfwGetWin32Window((GLFWwindow *)Application::Get().GetWindow().GetNativeWindow());
     ofn.lpstrFile = szFile;
     ofn.nMaxFile = sizeof(szFile);
+    if (GetCurrentDirectoryA(256, currentDir))
+        ofn.lpstrInitialDir = currentDir;
     ofn.lpstrFilter = filter;
     ofn.nFilterIndex = 1;
 
     ofn.lpstrDefExt = strchr(filter, '\0') + 1;
-    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR;
     if (GetSaveFileNameA(&ofn) == TRUE) {
         return ofn.lpstrFile;
     }
